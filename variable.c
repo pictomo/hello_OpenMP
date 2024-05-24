4fa8e24de6020110e490eb93445593b53cd77383
pictomo <tomo.pict@gmail.com>
2024-05-24 12:12:04 +0900

    [update]update example of variable, change detail

--- variable.c
+++ variable.c
@@ -3,10 +3,16 @@
 
 int main()
 {
+	int shared = 3;
 #pragma omp parallel
 	{
 		int thread_num = omp_get_thread_num();
-		printf("thread : %d\n", thread_num);
+		printf("thread : %d, shared : %d\n", thread_num, shared);
 	}
+#pragma omp parallel private(shared)
+	{
+		shared++;
+	}
+	printf("shared : %d\n", shared);
 	return 0;
 }
